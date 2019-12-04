89db2f2d6a60a39ef745d21c61dacc300591c385
luispecanha <luisgustavopxv@hotmail.com>
2019-12-04 07:54:51 -0300

    Fixed catioro page

--- Trab4-BackEndPetShop/routes/index.js
+++ Trab4-BackEndPetShop/routes/index.js
@@ -8,6 +8,9 @@ router.get('/', (req, res) => res.render('welcome'));
 // Welcome through logo
 router.get('/welcome', (req, res) => res.render('welcome'));
 
+// Catioro Page
+router.get('/cachorro', (req, res) => res.render('cachorro'));
+
 // Dashboard
 router.get('/dashboard', ensureAuthenticated, (req, res) => 
     res.render('dashboard', {
